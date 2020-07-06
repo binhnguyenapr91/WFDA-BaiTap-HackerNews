fc48c9aa39a89721ab6833609583c9cc19821659
binhnguyenapr91 <binhnguyen.apr91@gmail.com>
2020-07-06 17:18:33 +0700

    init commit

---
+++ src/app/mock-articles.ts
@@ -0,0 +1,34 @@
+import {Article} from './article';
+
+export const Articles: Article[] = [
+    {
+        id : 1,
+        title : 'Brython – A Python 3 implementation for client-side web programming',
+        url : 'https://brython.info/'
+    },
+    {
+        id : 2,
+        title : 'Brython – A Python 3 implementation for client-side web programming',
+        url : 'https://brython.info/'
+    },
+    {
+        id : 3,
+        title : 'Brython – A Python 3 implementation for client-side web programming',
+        url : 'https://brython.info/'
+    },
+    {
+        id : 4,
+        title : 'Brython – A Python 3 implementation for client-side web programming',
+        url : 'https://brython.info/'
+    },
+    {
+        id : 5,
+        title : 'Brython – A Python 3 implementation for client-side web programming',
+        url : 'https://brython.info/'
+    },
+    {
+        id : 6,
+        title : 'Brython – A Python 3 implementation for client-side web programming',
+        url : 'https://brython.info/'
+    }
+]
